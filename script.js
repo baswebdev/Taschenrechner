0275c93045f8816d22f84172f75bd4f0898128d1
Burak Akbas <127743530+baswebdev@users.noreply.github.com>
2023-10-15 21:40:38 +0200

    script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+function appendToResult(value) {
+    document.getElementById('result').value += value;
+}
+
+function clearResult() {
+    document.getElementById('result').value = '';
+}
+
+function calculateResult() {
+    const result = eval(document.getElementById('result').value);
+    document.getElementById('result').value = result;
+}
